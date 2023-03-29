bdf3babee7edcf80bff2e42eb48dd34ad74ee0c3
Camilo Solis <camilo.solis@est.uca.edu.ni>
2023-03-28 23:17:40 -0600

    Practica de visual studio y sql server

---
+++ Reportes/Reportes/Form1.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnImprimirCargos = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnImprimirCargos
+        '
+        Me.btnImprimirCargos.Font = New System.Drawing.Font("Microsoft Tai Le", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnImprimirCargos.Location = New System.Drawing.Point(285, 144)
+        Me.btnImprimirCargos.Name = "btnImprimirCargos"
+        Me.btnImprimirCargos.Size = New System.Drawing.Size(256, 129)
+        Me.btnImprimirCargos.TabIndex = 0
+        Me.btnImprimirCargos.Text = "Mostrar Reportes de Cargos"
+        Me.btnImprimirCargos.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnImprimirCargos)
+        Me.Name = "Form1"
+        Me.Text = "Cargos"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnImprimirCargos As Button
+End Class
---
+++ Reportes/Reportes/Form1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Data.SqlClient
+Imports Microsoft.Reporting.WinForms
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnImprimirCargos.Click
+
+    End Sub
+
+    Sub verReporte(ByVal t As DataTable, ByVal ds As String, ByVal nombreRpt As String)
+        Try
+            Dim rpt As New ReportDataSource(ds, t)
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Clear()
+            FrmVistaPrevia.ReportViewer1.LocalReport.DataSources.Add(rpt)
+            FrmVistaPrevia.ReportViewer1.LocalReport.ReportPath = nombreRpt
+            FrmVistaPrevia.ReportViewer1.Refresh()
+            FrmVistaPrevia.Show()
+
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al mostrar reporte")
+        End Try
+    End Sub
+
+    Private Sub btnImprimirCargos_Click(sender As Object, e As EventArgs) Handles btnImprimirCargos.Click
+        Try
+            Dim tsql As String = "Select * from jobs"
+            Dim conex As New SqlConnection(My.Settings.cStrBDHR)
+            Dim da As New SqlDataAdapter(tsql, conex)
+            Dim t As New DataTable
+            da.Fill(t)
+            verReporte(t, "dsRegistros", "diseniosRpt\rptCargo.rdlc")
+        Catch ex As Exception
+            MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al cargar reporte")
+
+        End Try
+    End Sub
+
+
+End Class
